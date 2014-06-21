b1d1d6f662de9f50baab6444b613b11e3f423cb2
Ikuo Matsumura <makiczar@gmail.com>
2014-06-21 22:13:10 +0900

    Created project: android create project -t 19 -k com.github.ikuo.android_error_dialog -a MainActivity -n "AndroidErrorDialog" -p android-error-dialog

---
+++ src/com/github/ikuo/android_error_dialog/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.ikuo.android_error_dialog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
